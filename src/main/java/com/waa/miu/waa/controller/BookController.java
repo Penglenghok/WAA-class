8af9016a67cb73388f695ae06ce7659821222323
pengleng <jk6666depro@gmail.com>
2024-10-04 14:41:03 -0500

    W1D5

--- src/main/java/com/waa/miu/waa/controller/BookController.java
+++ src/main/java/com/waa/miu/waa/controller/BookController.java
@@ -1,6 +1,8 @@
 package com.waa.miu.waa.controller;
 
 import com.waa.miu.waa.model.Book;
+import com.waa.miu.waa.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,17 +14,23 @@ import java.util.List;
 @RequestMapping("/books")
 public class BookController {
 
-    List<Book> books = new ArrayList<>();
+    private BookRepository bookRepository;
+
+    @Autowired
+    public BookController(BookRepository bookRepository){
+        this.bookRepository=bookRepository;
+    }
 
     @GetMapping(produces = "application/vnd.books.v1+json")
     public ResponseEntity<List<Book>> getBook() {
-        return ResponseEntity.status(HttpStatus.ACCEPTED).body(this.books);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(this.bookRepository.findAll());
     }
 
     @PostMapping
     public ResponseEntity<Book> addBook(@RequestHeader("X-API-VERSION") String version, @RequestBody Book book) {
-        if (version == "2") {
-            this.books.add(book);
+        System.out.println(version.equals("2"));
+        if (version.equals("2")) {
+            this.bookRepository.save(book);
             return ResponseEntity.status(HttpStatus.ACCEPTED).body(book);
         }
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(null);
@@ -30,27 +38,27 @@ public class BookController {
 
     @GetMapping("/v1/{id}")
     public ResponseEntity<Book> addBook(@PathVariable int id) {
-        Book book = this.books.stream().filter(x -> x.getId() == id).findFirst().get();
+        Book book = this.bookRepository.findById(id).get();
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(book);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Book> putBook(@RequestParam("version") String version, @PathVariable int id, @RequestBody Book book) {
         if (version == "2") {
-            Book currentBook = this.books.stream().filter(x -> x.getId() == id).findFirst().get();
+            Book currentBook = this.bookRepository.findById(id).get();
             currentBook.setTitle(book.getTitle());
             currentBook.setIsbn(book.getIsbn());
             currentBook.setPrice(book.getPrice());
+            this.bookRepository.save(currentBook);
             return ResponseEntity.status(HttpStatus.ACCEPTED).body(currentBook);
         }
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(null);
     }
 
     @DeleteMapping
-    public ResponseEntity<Book> deleteBook(@PathVariable int id, @RequestBody Book book) {
-        Book currentBook = this.books.stream().filter(x -> x.getId() == id).findFirst().get();
-        this.books.remove(currentBook);
-        return ResponseEntity.status(HttpStatus.ACCEPTED).body(currentBook);
+    public ResponseEntity<Book> deleteBook(@PathVariable int id) {
+        this.bookRepository.deleteById(id);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(null);
     }
 
 }
--- src/main/java/com/waa/miu/waa/model/Book.java
+++ src/main/java/com/waa/miu/waa/model/Book.java
@@ -1,13 +1,19 @@
 package com.waa.miu.waa.model;
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
 @Data
 public class Book {
+    @Id
     private int id;
     private String title;
     private String isbn;
