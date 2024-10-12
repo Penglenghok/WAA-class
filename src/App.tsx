1d4a32d979715114cbbe4dba936683258fc60388
pengleng <jk6666depro@gmail.com>
2024-10-12 10:31:08 -0500

    mock json server

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.scss";
 import Comment from "./components/Comment";
 import avatar from "./images/bozai.png";
 import { orderBy } from "lodash";
 import moment from "moment";
 
-// Comment List data
-const defaultList = [
-  {
-    // comment id
-    rpid: 3,
-    // user info
-    user: {
-      uid: "13258165",
-      avatar: "",
-      uname: "Jay Zhou",
-    },
-    // comment content
-    content: "Nice, well done",
-    // created datetime
-    ctime: "10-18 08:15",
-    like: 88,
-  },
-  {
-    rpid: 2,
-    user: {
-      uid: "36080105",
-      avatar: "",
-      uname: "Song Xu",
-    },
-    content: "I search for you thousands of times, from dawn till dusk.",
-    ctime: "11-13 11:29",
-    like: 88,
-  },
-  {
-    rpid: 1,
-    user: {
-      uid: "30009257",
-      avatar,
-      uname: "John",
-    },
-    content:
-      "I told my computer I needed a break... now it will not stop sending me vacation ads.",
-    ctime: "10-19 09:00",
-    like: 66,
-  },
-];
-// current logged in user info
 const user = {
-  // userid
   uid: "30009257",
-  // profile
   avatar,
-  // username
   uname: "John",
 };
 
-// Nav Tab
 const tabs = [
   { type: "hot", text: "Top" },
   { type: "newest", text: "Newest" },
 ];
 
 const App = () => {
-  const [comments, setcomments] = useState(defaultList);
+  const [comments, setcomments] = useState<Array<any>>([]);
   const [content, setcontent] = useState("");
 
   const [currentFilter, setcurrentFilter] = useState<string>("");
+
+  useEffect(() => {
+    fetch("http://localhost:3004/comments").then((response) => {
+      response.json().then((data) => {
+        setcomments(data);
+      });
+    });
+  }, []);
 
   const onFilter = (filter: string) => {
     setcurrentFilter(filter);
@@ -105,7 +67,7 @@ const App = () => {
           <li className="nav-title">
             <span className="nav-title-text">Comments</span>
             {/* Like */}
-            <span className="total-reply">{defaultList.length}</span>
+            <span className="total-reply">{comments.length}</span>
           </li>
           <li className="nav-sort">
             {/* highlight class name： active */}
